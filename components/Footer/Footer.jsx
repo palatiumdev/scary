2c957c5dba934c09c8c18d9908682bf08c3a0211
palatiumdev <palatiumwebdesign@gmail.com>
2024-08-16 13:09:37 +0200

    font-mono

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
             <div className="text-center text-background">
                 <h1 className="text-[20rem] font-extrabold ">VFXPJB</h1>
             </div>
-            <div className="absolute bottom-8 inset-x-0 text-center">
-                <p className="text-base">VFXPJB</p>
-                <p className="text-base">Copyright 2024. VFXPJB - All Rights Reserved</p>
+            <div className="absolute bottom-8 inset-x-0 text-center ">
+                <p className="text-base font-mono">VFXPJB</p>
+                <p className="text-base font-mono">Copyright 2024. VFXPJB - All Rights Reserved</p>
             </div>
         </div>
     )
