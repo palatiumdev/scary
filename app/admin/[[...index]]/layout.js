05ff8acc9418f6ad9ac6d35fc779d175429d22f8
palatiumdev <palatiumwebdesign@gmail.com>
2024-09-20 23:24:10 +0200

    first commit

---
+++ app/admin/[[...index]]/layout.js
@@ -0,0 +1,43 @@
+
+import { getMetadata } from '@/sanity/sanity-utils';
+
+
+export default async function RootLayout({ children }) {
+
+    return (
+        <html lang="en">
+            <body className={`${tommy.variable} ${regular.variable} font-sans bg-background  text-white`} >
+                {children}
+            </body>
+        </html>
+    );
+}
+
+export async function generateMetadata({ params }) {
+    const metadataContent = await getMetadata();
+    return {
+        title: metadataContent[0]?.websiteTitle,
+        description: metadataContent[0]?.description,
+        openGraph: {
+            title: metadataContent[0]?.websiteTitle,
+            description: metadataContent[0]?.description,
+            url: metadataContent[0]?.websiteTitlesiteUrl,
+            images: [
+                {
+                    url: metadataContent[0]?.embedBanner,
+                    width: 800,
+                    height: 600,
+                }
+            ],
+        },
+        twitter: {
+            images: [
+                {
+                    url: metadataContent[0]?.embedBanner,
+                    width: 800,
+                    height: 600,
+                },
+            ],
+        }
+    }
+}
