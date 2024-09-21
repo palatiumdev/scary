a54dd005e3339dd372dc0b5b05ab7ba20b7ce175
palatiumdev <palatiumwebdesign@gmail.com>
2024-09-22 01:04:54 +0200

    loading test 5

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -6,7 +6,7 @@ import { TbArrowBigRightFilled } from "react-icons/tb";
 const Button = ({ text }) => {
     return (
         <div className="bg-primary rounded-3xl text-4xl font-extrabold w-full px-10 py-5  grid place-items-center">
-            <button onClick={() => { window.open("https://x.com/vfxpjb", "_blank") }} className="flex gap-4 items-center">
+            <button onClick={() => { window.open("https://x.com/ScaryAM__", "_blank") }} className="flex gap-4 items-center">
                 <p>{text}</p>
                 <TbArrowBigRightFilled />
             </button>
