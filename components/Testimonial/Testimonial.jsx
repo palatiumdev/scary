968db3ec6b17d643676bf3541fa20e16a668348e
palatiumdev <palatiumwebdesign@gmail.com>
2024-09-25 22:43:54 +0200

    loading test 5

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -5,14 +5,15 @@ import { FaStar } from "react-icons/fa";
 const Testimonial = async ({ channelId, testimonial }) => {
     const user = await getCreator(channelId)
 
+
     return (
         <div className="bg-accent/50 rounded-3xl p-10 grid gap-4 max-w-[40rem] 2xl:w-[40rem] min-h-fit">
             <div className="flex w-full justify-between	lg:gap-8 lg:flex-row flex-col gap-4">
                 <div className="flex items-center gap-4">
                     <Image
                         src={user.profileImage}
-                        width={"60"}
-                        height={"60"}
+                        width={testimonial ? "60" : "80"}
+                        height={testimonial ? "60" : "80"}
                         className="rounded-full"
                     />
                     <div>
@@ -21,15 +22,8 @@ const Testimonial = async ({ channelId, testimonial }) => {
                     </div>
 
                 </div>
-                {/*                 <div className="flex text-2xl gap-2 text-yellow-300  pt-[5px]">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                </div>
- */}            </div>
-            <p className="text-2xl font-mono text-wrap">{testimonial}</p>
+            </div>
+            {testimonial && <p className="text-2xl font-mono text-wrap">{testimonial}</p>}
         </div>
     )
 }
