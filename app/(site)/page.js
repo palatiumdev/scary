50fec763c17a65d6dd5427ffb469e03f84c31148
palatiumdev <palatiumwebdesign@gmail.com>
2024-09-27 08:00:47 +0200

    loading test 5

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -48,7 +48,7 @@ export default async function Home() {
 
       <div className="grid place-items-center w-full gap-8">
         <h1 className="text-5xl text-primary">Work</h1>
-        <VideoTestimonial videos={home[0]?.videos} />
+        <ShortTestimonial videos={home[0]?.videos} />
       </div>
 
       <div className="w-full flex flex-col items-center gap-16">
