68a45c07b04619b0e66abb91a57599c0e107ca75
palatiumdev <palatiumwebdesign@gmail.com>
2024-11-24 15:54:42 +0100

    loading test 5

--- components/Showcase/Showcase.jsx
+++ components/Showcase/Showcase.jsx
@@ -2,14 +2,19 @@ import Testimonial from "../Testimonial/Testimonial"
 import Video from "../Video/Video"
 
 const Showcase = ({ videos, text, channelId, testimonial, }) => {
-
+    let videoCols = videos.length - 1;
+    if (videoCols > 3) {
+        videoCols = 3;
+    } else if (videoCols < 2) {
+        videoCols = 2;
+    }
     return (
         <div className="grid xl:grid-cols-2 w-full px-5 lg:px-56 gap-16 place-items-center">
             <div className="grid gap-8 w-full">
                 <div>
                     <Video videoId={videos[0]?.videoId} />
                 </div>
-                <div className={`w-full grid gap-8`} style={{ gridTemplateColumns: `repeat(${videos.length - 1}, minmax(0, 1fr));`, }}>
+                <div className={`w-full grid gap-8`} style={{ gridTemplateColumns: `repeat(${videoCols}, minmax(0, 1fr));`, }}>
 
                     {videos.map((video, i) => {
                         if (i != 0) {
