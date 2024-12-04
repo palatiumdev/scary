4a09a0fd0d968bf82c7a62a31deeb11594ff3b73
palatiumdev <palatiumwebdesign@gmail.com>
2024-12-04 22:48:06 +0100

    change

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,19 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/clients', // Matches the /clients page
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
